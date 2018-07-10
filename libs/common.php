d45a572ce4eb17a20298019a5b45ab9d8ad9f691
demel42 <demel42@web.de>
2018-07-10 08:17:49 +0200

    GetValue() ist now 'protected'

--- libs/common.php
+++ libs/common.php
@@ -20,7 +20,7 @@ trait AutomowerCommon
         }
     }
 
-    private function GetValue($Ident)
+    protected function GetValue($Ident)
     {
         @$varID = $this->GetIDForIdent($Ident);
         if ($varID == false) {
