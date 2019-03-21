d5ce5f76d7d49037941ad2ddc12657b4eac04cca
demel42 <demel42@web.de>
2019-03-21 19:37:20 +0000

    Apply fixes from StyleCI

--- libs/common.php
+++ libs/common.php
@@ -25,7 +25,7 @@ trait AutomowerCommon
             return;
         }
 
-		$ret = parent::SetValue($Ident, $Value);
+        $ret = parent::SetValue($Ident, $Value);
         if ($ret == false) {
             $this->SendDebug(__FUNCTION__, 'mismatch of value "' . $Value . '" for variable ' . $Ident, 0);
         }
@@ -39,7 +39,7 @@ trait AutomowerCommon
             return false;
         }
 
-		$ret = parent::GetValue($Ident);
+        $ret = parent::GetValue($Ident);
         return $ret;
     }
 
