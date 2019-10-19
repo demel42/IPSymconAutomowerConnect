8afdd1bd642c0935deb20148a2db5aee9e2bb5f2
IP-Symcon <root@ips-prod>
2019-10-19 10:05:08 +0200

    update .style

--- docs/GetRawData2GoogelMaps.php
+++ docs/GetRawData2GoogelMaps.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 // Beispiel zur Datestellung der GPS-Ќoordinaten mit Hilfe von GooleMaps (siehe https://github.com/demel42/IPSymconGoogleMaps.git)
 // in dem Script verwendete Objekt-ID's durch die eigenen ID's ersetzen
 
@@ -9,9 +11,9 @@ $jdata = json_decode($cdata, true);
 $points = [];
 foreach ($jdata as $jpoint) {
     $points[] = [
-            'lat' => $jpoint['latitude'],
-            'lng' => $jpoint['longitude'],
-        ];
+        'lat' => $jpoint['latitude'],
+        'lng' => $jpoint['longitude'],
+    ];
 }
 
 // allgemeine Angaben zur Karte
@@ -30,9 +32,9 @@ $markers = [];
 $marker_points = [];
 
 $point = [
-        'lat' => GetValueFloat(17701 /*[Rasenmäher\Automower\letzter Breitengrad]*/),
-        'lng' => GetValueFloat(40825 /*[Rasenmäher\Automower\letzter Längengrad]*/),
-    ];
+    'lat' => GetValueFloat(17701 /*[Rasenmäher\Automower\letzter Breitengrad]*/),
+    'lng' => GetValueFloat(40825 /*[Rasenmäher\Automower\letzter Längengrad]*/),
+];
 
 $marker_points[0] = $point;
 
@@ -41,19 +43,19 @@ $activity_label = ['E', 'D', 'P', 'L', 'S', 'F', 'M'];
 $label = $activity_label[$activity + 1];
 
 $markers[] = [
-        'color'     => 'green',
-        'label'		   => $label,
-        'points'    => $marker_points,
-    ];
+    'color'     => 'green',
+    'label'		   => $label,
+    'points'    => $marker_points,
+];
 
 $map['markers'] = $markers;
 
 $paths = [];
 $paths[] = [
-        'color'     => '0xff0000ff',       // 0xhhhhhhoo oo=opacity
-        'weight'    => 2,
-        'points'    => $points,
-    ];
+    'color'     => '0xff0000ff',       // 0xhhhhhhoo oo=opacity
+    'weight'    => 2,
+    'points'    => $points,
+];
 
 $map['paths'] = $paths;
 
