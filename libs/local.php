5dfc4d97d4daaf34f34d3aacf4feeaca31857efd
Ch. Damsky <demel41@web.de>
2022-04-20 11:50:34 +0200

    IS_NOSYMCONCONNECT ist nun in CommonStubs

--- libs/local.php
+++ libs/local.php
@@ -8,11 +8,10 @@ trait AutomowerConnectLocalLib
     public static $IS_SERVERERROR = IS_EBASE + 11;
     public static $IS_HTTPERROR = IS_EBASE + 12;
     public static $IS_INVALIDDATA = IS_EBASE + 13;
-    public static $IS_NOSYMCONCONNECT = IS_EBASE + 14;
-    public static $IS_NOLOGIN = IS_EBASE + 15;
-    public static $IS_FORBIDDEN = IS_EBASE + 16;
-    public static $IS_INVALIDACCOUNT = IS_EBASE + 17;
-    public static $IS_DEVICE_MISSING = IS_EBASE + 18;
+    public static $IS_NOLOGIN = IS_EBASE + 14;
+    public static $IS_FORBIDDEN = IS_EBASE + 15;
+    public static $IS_INVALIDACCOUNT = IS_EBASE + 16;
+    public static $IS_DEVICE_MISSING = IS_EBASE + 17;
 
     private function GetFormStatus()
     {
@@ -22,7 +21,6 @@ trait AutomowerConnectLocalLib
         $formStatus[] = ['code' => self::$IS_SERVERERROR, 'icon' => 'error', 'caption' => 'Instance is inactive (server error)'];
         $formStatus[] = ['code' => self::$IS_HTTPERROR, 'icon' => 'error', 'caption' => 'Instance is inactive (http error)'];
         $formStatus[] = ['code' => self::$IS_INVALIDDATA, 'icon' => 'error', 'caption' => 'Instance is inactive (invalid data)'];
-        $formStatus[] = ['code' => self::$IS_NOSYMCONCONNECT, 'icon' => 'error', 'caption' => 'Instance is inactive (no Symcon-Connect)'];
         $formStatus[] = ['code' => self::$IS_NOLOGIN, 'icon' => 'error', 'caption' => 'Instance is inactive (not logged in)'];
         $formStatus[] = ['code' => self::$IS_FORBIDDEN, 'icon' => 'error', 'caption' => 'Instance is inactive (forbidden)'];
         $formStatus[] = ['code' => self::$IS_INVALIDACCOUNT, 'icon' => 'error', 'caption' => 'Instance is inactive (invalid account)'];
